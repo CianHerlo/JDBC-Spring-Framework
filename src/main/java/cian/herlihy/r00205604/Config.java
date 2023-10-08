b7436fe881aa2a7a960e1274638e5ba884810d2a
Cian Herlihy <cianherlihy2@gmail.com>
2023-10-08 14:42:52 +0100

    Initial set up of project

---
+++ src/main/java/cian/herlihy/r00205604/Config.java
@@ -0,0 +1,24 @@
+package cian.herlihy.r00205604;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+public class Config {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:data.sql").build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
---
+++ src/main/java/cian/herlihy/r00205604/Main.java
@@ -0,0 +1,7 @@
+package cian.herlihy.r00205604;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
